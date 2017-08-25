fcf4b2e30c200d75119f400a9eb86de277c44b09
Felix Schinkel <*>
2017-08-25 13:10:00 +0200

    meal prices are now shown correctly

--- helmut/telegramCommandands.py
+++ helmut/telegramCommandands.py
@@ -1,4 +1,11 @@
+import logging
 import apiParser
+
+
+def get_price(price):
+    if price is None:
+        return ""
+    return format(price, '.2f') + ' €'
 
 
 def vegan_command(bot, update):
@@ -9,18 +16,13 @@ def vegan_command(bot, update):
     for canteen in canteens:
         for meal in canteen['meals']:
             if any("vegan" in n for n in meal['notes']):
-                price = meal['prices']['students']
-                if price is None:
-                    price = ""
-                else:
-                    price = str(price) + " €"
-                result += "{}\n\t -{} {}\n".format(canteen['name'], meal['name'], price)
+                result += "{}\n\t -{} {}\n".format(canteen['name'], meal['name'], get_price(meal['prices']['students']))
 
     # should never happen, but just in case...
     if not result:
         result = "Nischt veganes heut!"
-
     bot.send_message(chat_id=update.message.chat_id, text=result)
+    logging.info("Send /vegan to id: {} with text: {}".format(update.message.chat_id, result))
 
 
 def get_meals_for_canteen(bot, update, args):
@@ -32,13 +34,9 @@ def get_meals_for_canteen(bot, update, args):
         if args[0].lower() in canteen['name'].lower():
             result += "{}:\n\t".format(canteen['name'])
             for meal in canteen['meals']:
-                price = meal['prices']['students']
-                if price is None:
-                    price = ""
-                else:
-                    price = str(price) + " €"
-                result += "-{} {}\n\t".format(meal['name'], price)
+                result += "-{} {}\n\t".format(meal['name'], get_price(meal['prices']['students']))
 
     if not result:
         result = "Konnte für {} keine Mensa finden. Spasst".format(args[0])
     bot.send_message(chat_id=update.message.chat_id, text=result)
+    logging.info("Send /essen {} to id: {} with text: {}".format(args[0], update.message.chat_id, result))
