c2a0974af343894c1817b4709a41b60c4f134194
Felix Schinkel <FelixSchinkel@users.noreply.github.com>
2017-08-27 12:45:40 +0200

    updated response, when there is no or an false API Key

--- helmut/mainBot.py
+++ helmut/mainBot.py
@@ -1,18 +1,35 @@
 import logging
 import sys
+
+import telegram
 from telegram.ext import Updater, CommandHandler
 
 import telegramCommandands
+
+USAGE_TEXT = """
+
+Usage:
+    python mainBot.py API_KEY
+"""
 
 
 def main():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
     if len(sys.argv) < 2:
-        print("Token needed!! Aborting")
+        print("Api Key needed!" + USAGE_TEXT)
         return
 
+    # check if valid API Token is applied
     updater = Updater(sys.argv[1])
+    try:
+        status = updater.bot.getMe()
+    except telegram.TelegramError:
+        print("Api Key not valid!" + USAGE_TEXT)
+        return
+    else:
+        logging.info("Authenticated as: {}".format(status['username']))
+
     dispatcher = updater.dispatcher
 
     meal_handler = CommandHandler("essen", telegramCommandands.get_meals_for_canteen, pass_args=True)
